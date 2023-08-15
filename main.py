2db5c20e8f4e21ef81cdcc784c37a7d70056bf44
James Huang <13161133872@163.com>
2023-08-15 16:26:18 +0800

    1143 LCS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from solution import Solution
 
 llist = [0, 1, 0, 3, 2, 3]
-a = 1011
-print(Solution().lengthOfLIS(llist))
+a = "oxcpqrsvwf"
+b = "shmtulqrypy"
+print(Solution().longestCommonSubsequence(a, b))
--- solution.py
+++ solution.py
@@ -71,6 +71,7 @@ class Solution:
         return dp[amount] if dp[amount] != float('inf') else -1
 
     # 300
+    # DOT NOT UNDERSTAND DICHOTOMY
     def lengthOfLIS(self, nums: List[int]) -> int:
         dp = [1]
         for i in range(len(nums)):
@@ -93,3 +94,25 @@ class Solution:
         #     max_length = max(length, max_length)
         #     print(1, ": ", max_length)
         # return max_length
+
+    # 1143
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = []
+        # generate 2d array
+        # for i in range(m + 1):
+        #     dp.append([])
+        #     for j in range(n + 1):
+        #         dp[i].append(0)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        # fill in 2d array
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        # print out dp
+        for i in range(len(dp)):
+            print(dp[i])
+        return dp[m][n]
